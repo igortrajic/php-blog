08283fdc70b1564658831e5fe15101022f4bf228
igortrajic <iggetrajic@gmail.com>
2026-02-13 07:55:55 +0100

    chore: gemini suggestions

--- database/schema.sql
+++ database/schema.sql
@@ -1,16 +1,16 @@
 create table users (
     id integer primary key autoincrement,
     name varchar(200) not null,
-    email varchar(255) not null,
-    password varchar(100) not null
+    email varchar(255) not null unique,
+    password text not null
 );
 
 create table posts (
     id integer primary key autoincrement,
     title varchar(200) not null,
     image text,
-    content varchar(255),
+    content text,
     created_at datetime default current_timestamp,
     user_id integer,
-    foreign key (user_id) references users(id)
+    foreign key (user_id) references users(id) on delete cascade
 );
