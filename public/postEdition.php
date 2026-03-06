64d01e9fd493c485fb50ddf5871151c6ddf9bc5e
igortrajic <iggetrajic@gmail.com>
2026-03-06 10:27:21 +0100

    feat: post details showing when clicking on a specific post
    also added some error management for missing id or post for edit
    
    closes #8

--- public/postEdition.php
+++ public/postEdition.php
@@ -7,9 +7,17 @@ $db = Database::getConnection();
 
 $id = $_REQUEST['id'] ?? null; 
 
+if (!$id) {
+    die("Error: No ID received. Make sure the Edit link looks like: postEdition.php?id=123");
+}
+
 $id = (int)$id;
 
 $post = Post::getPostById($db, $id);
+
+if (!$post) {
+    die("Error: Post not found in database.");
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = trim($_POST['title'] ?? '');
@@ -41,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             $finfo = new finfo(FILEINFO_MIME_TYPE);
             $realMimeType = $finfo->file($tmpName);
-            $allowedMimeTypes =  ['image/jpeg', 'image/png'];
+            $allowedMimeTypes =  ['image/jpeg', 'image/png', 'image/jpg'];
 
             if (!in_array($extension, $allowedExtensions) || !in_array($realMimeType, allowedMimeType)){
                 $message = "Error: Invalid image file.";
@@ -50,11 +58,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
 
-        if ($updatedPost->update($db, $id)) {
+        if (empty($title) || empty($content)) {
+            $message = "Title and content are required!";
+        } elseif ($uploadSuccess) {
+            $updatedPost = new Post([
+                'title' => $title,
+                'content' => $content,
+                'image' => $imagePath
+            ]);
+
+        if ($imagePath !== $post['image']) {
+            $uploadSuccess = $updatedPost->moveFile($_FILES['fileToUpload']['tmp_name']);
+        }
+
+        if ($uploadSuccess && $updatedPost->update($db, $id)) {
             header("Location: allPosts.php?message=Updated successfully");
             exit;
+        } elseif (!$uploadSuccess) {
+            $message = "Failed to move the uploaded image.";
         } else {
             $message = "Database error: Could not save changes.";
+        }
         }
     }
 }
